d726c09faa0e1e17c15d76f7c7d332bd6e7e22b9
ed <sfuptown@live.com>
2014-08-26 22:27:18 -0700

    add comments

--- AspNet.Identity.BLToolkit/Globals.cs
+++ AspNet.Identity.BLToolkit/Globals.cs
@@ -8,6 +8,10 @@ namespace AspNet.Identity.BLToolkit
 {
   public  class Globals
     {
+      /// <summary>
+      /// The connection string from web.config
+      /// Set this to the name of the conn string you use for your database
+      /// </summary>
       public static string ConnString = "DefaultConnection";
     }
 }
